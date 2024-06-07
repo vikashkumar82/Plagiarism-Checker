90286c756b76b9540762c0e2423e1dbcaf45d6b4
vikashkumar82 <avikashkumar82@gmail.com>
2024-06-07 13:26:01 +0530

    Add project files.

---
+++ index.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="plagGarism.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Style/StyleSheet.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+    <script src="https://kit.fontawesome.com/8e1d3b8195.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <div class="container">
+            <section>
+                <div class="row">
+                    <div class="col-lg-9 col-md-12">
+                        <div class="plagarism-hero mt-5">
+                            <center>
+                                <h1>Palagarism Checker</h1>
+                            </center>
+                            <div class="plagarism-main p-3">
+                                <asp:TextBox ID="txtInput" runat="server" TextMode="MultiLine" Rows="10" Columns="50" CssClass="form-control" placeholder="Please enter your text and press Check Plagiarism"></asp:TextBox>
+                                <br />
+                                <center>
+                                    <asp:Button ID="btnCheck" runat="server" Text="Check for Plagiarism" OnClick="btnCheck_Click" CssClass="btn btn-success" /></center>
+                                <asp:Repeater ID="rptResults" runat="server">
+                                    <ItemTemplate>
+                                        <div class="result-repeater">
+                                            <p>
+                                                <strong style="color: grey">Similarity:</strong> <%# Eval("Similarity") %>
+                                                <p style="color: #2B89E2; line-break: anywhere;"><%# Eval("Link") %></p>
+                                                <%# Eval("Snippet") %><br />
+
+                                                <a href='<%# Eval("Link") %>' target="_blank" style="text-decoration: none; color: #3CCA7B; line-break: anywhere;"><%# Eval("Link") %>&nbsp;&nbsp;<span><i class="fa-solid fa-arrow-up-right-from-square"></i></span></a><br />
+                                            </p>
+                                        </div>
+                                    </ItemTemplate>
+                                </asp:Repeater>
+                                <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-12"></div>
+                </div>
+
+            </section>
+        </div>
+
+
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace plagGarism
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCheck_Click(object sender, EventArgs e)
+        {
+            string inputText = txtInput.Text;
+            List<PlagiarismResult> results = CheckPlagiarism(inputText);
+
+            if (results.Count > 0)
+            {
+                rptResults.DataSource = results;
+                rptResults.DataBind();
+                lblResult.Text = "Potential plagiarism detected. Similar content found.";
+            }
+            else
+            {
+                lblResult.Text = "No plagiarism detected. Content is unique.";
+            }
+        }
+
+        public List<PlagiarismResult> CheckPlagiarism(string text)
+        {
+            string apiKey = "AIzaSyAACsjtFAuk377IWjF23cDSBNjFyIGmwhU";
+            string searchEngineId = "663aad86da6d34869";
+            string query = System.Net.WebUtility.UrlEncode(text);
+            string apiUrl = $"https://www.googleapis.com/customsearch/v1?q={query}&key={apiKey}&cx={searchEngineId}";
+
+            List<PlagiarismResult> results = new List<PlagiarismResult>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(apiUrl).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = response.Content.ReadAsStringAsync().Result;
+                    JObject searchResults = JObject.Parse(jsonResponse);
+                    var items = searchResults["items"];
+                    if (items != null)
+                    {
+                        foreach (var item in items)
+                        {
+                            string link = item["link"].ToString();
+                            string snippet = item["snippet"].ToString();
+
+                            // Calculate similarity
+                            string similarity = GetSimilarityPercentage(text, snippet);
+
+                            results.Add(new PlagiarismResult { Link = link, Snippet = snippet, Similarity = similarity });
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        private string GetSimilarityPercentage(string text1, string text2)
+        {
+            // Normalize the texts by converting to lower case and removing punctuation
+            text1 = NormalizeText(text1);
+            text2 = NormalizeText(text2);
+
+            // Split the texts into words
+            string[] words1 = text1.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] words2 = text2.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Calculate the number of exact word matches
+            int matchCount = words1.Intersect(words2).Count();
+
+            // Calculate the similarity percentage
+            double similarityPercentage = (double)matchCount / words1.Length * 100;
+
+            // Format the percentage to two decimal places
+            return similarityPercentage.ToString("F2") + "%";
+        }
+
+        private string NormalizeText(string text)
+        {
+            // Convert to lower case and remove punctuation using regex
+            text = text.ToLower();
+            text = Regex.Replace(text, @"[^\w\s]", "");
+            return text;
+        }
+    }
+
+    public class PlagiarismResult
+    {
+        public string Link { get; set; }
+        public string Snippet { get; set; }
+        public string Similarity { get; set; }
+    }
+}
